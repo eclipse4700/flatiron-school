d58fb7add7a4066d2d8ef0d9765c4e50a83ddc01
Isaiah <isaiahkantor23@gmail.com>
2024-06-04 13:59:17 -0400

    Changed Today

--- phase-1/fundamentals/introduction/beginning/index.js
+++ phase-1/fundamentals/introduction/beginning/index.js
@@ -15,27 +15,170 @@ Core Deliverables
 // ~ Variables
 // -> constants, variables, logging, annotations
 
+// constant = a variable that will not change its value
+console.log("Hello World!");
+const firstName = "Isaiah";
+
+// let = a variable that can change value i.e. can be reassigned
+let number = 5;
+number = 6;
+console.log(number);
+
 // ~ Types
 // -> undefined, null*, boolean, number, string
+// Booleans are true or false
+const IsItRaining = false;
 
 // ~ Objects
 // -> definition, bracket/dot notation, stringify
 
+// An object named `dog` with a string property `firstName` and a number property `age`.
+const dog = {
+    firstName: "Fluffy",
+    age: 4
+};
+
+console.log(dog.firstName);
+console.log(dog["firstName"]);
+
+dog.age = 5;
+console.log(dog.age);
+console.log(JSON.stringify(dog));
+
 // ~ Arrays
 // -> definition, access, modification
 
+const prices = [30, 40, 10, 60];
+
+console.log(prices[1]);
+prices[2] = 20;
+console.log(prices[2]);
+
 // ~ Conditionals
 // if, if-else, if-else-if-else, ternary
+const isItRaining = true;
+
+// If it's raining, console.log() something about raining 
+if (isItRaining === true) {
+    console.log("Hey bring an unbrella!");
+} else {
+    console.log("Enjoy the beautiful day!");
+}
+
+const age = 21;
+
+if (age >= 21) {
+    console.log("You are of age");
+} else {
+    console.log("You are not of age")
+}
+
+//If the user is over 21 and it's raining, the log "21R"
+//If the user is over 21 and it's not raining, then log "21".
+//If the user is less and it's raining, log "YR"
+//If the user is less and it's  not raining, log "Y"
+
+const user = 22;
+
+
+if (user >= 21 && isItRaining === true) {
+    console.log("21R");
+} else if (user >= 21 && isItRaining === false){
+    console.log("21");
+} else if (user <= 21 && isItRaining === true){
+    console.log("YR"); 
+} else if (user <= 21 && isItRaining === false){
+    console.log("Y");
+}
+
+//OR
+
+if (user >= 21 && isItRaining) console.log("21R");
+if (user >= 21 && !isItRaining) console.log("21");
+if (user < 21 && isItRaining) console.log("YR");
+if (user < 21 && !isItRaining) console.log("YR");
+
+let message = "";
+if (user >= 21) message = "21";
+else message = "Y";
+
+if (isItRaining) message += "R";
+console.log(message);
+
 
 // ~ Functions
 // -> name, parameters, body, return, annotations
 // -> methods, forEach, callback functions, anonymous functions
 
+//greet "Hello, Sakib" ==> "Hello, Sakib!"
+
+function greet(greeting, recipient) {
+    //string interpolation
+    console.log(`${greeting}, ${recipient}!`);
+    //string concatenation 
+    //console.log(greetings + "," + recipient "!");
+    return (greeting + "," + recipient + "!");
+}
+
+const greetingIsaiah = greet("Hello", "Isaiah");
+console.log(greetingIsaiah)
+greet("Hello", "Jake")
+
+
+const words = ["jump","escalate","people"];
+
+//function printWord(word) {
+//    console.log(word);
+// }
+
+//Iterate over the array words
+words.forEach((word) => {console.log(word)});
+
 // ~ Scope
 // -> global, local, closures, function hoisting
+const a = "A"; //Global
+function localFunction(){
+    const a = "AA";
+    console.log(a);
+    const b = "B";
+    console.log(b);
+}
+
+localFunction();
+console.log(a);
+
+
+
 
 // CHALLENGES
 // Try these practice problems on your own to reinforce this lesson's material :)
 // 1. Write a function named `sum` that takes an array of `numbers` and returns its sum.
+
+const numbers = [10, 20, 30, 40];
+function sum(numbers){
+    let sum = 0;
+    numbers.forEach((number) => (sum += number));
+    return sum
+}
+
+console.log(sum(numbers));
+
 // 2. Write a function named `double` that takes an array of `numbers` and doubles each of its values.
+
+const numbers2 = [1,2,3];
+function double(numbers2){
+    let double = [];
+    double.forEach((number) => (double*2));
+    return double
+}
+
+console.log(double(numbers2));
+
+
+
+
 // 3. Write a function named `lowercase` that takes an array of `words` and returns a lowercased copy.
+
+
+
+
