57318fd24db1581eb94359226cf7a3b1fae2ab1d
Isaiah <isaiahkantor23@gmail.com>
2024-06-05 10:37:56 -0400

    Done today

--- phase-1/manipulation/beginning/index.js
+++ phase-1/manipulation/beginning/index.js
@@ -18,21 +18,32 @@ Challenges
 */
 
 // ~ APIs, CRUD, `document`
+// Document is an API that lets us interact (CRUD) with HTML!
 
 // ~ Read/Select a node
 // -> querySelector is a DOM method that lets us look up nodes by CSS **selector**.
 // -> querySelectorAll is a DOM method that returns an array-like list of nodes that match a CSS selector.
 // -> textContent is a property of text nodes (e.g. h1, p) that contain their text.
+console.log(document.querySelector("#tomorrow"));
+console.log(document.querySelector("#forever").textContent);
 
 
 // ~ Update/Modify a node's attributes
 // -> To modify an attribute, just use = after the attribute name in object dot notation
+document.querySelector("#forever").textContent = "Forever!!";
 
 // ~ Delete/Remove a node
 // -> To remove an existing element, we can look it up and call the node's method `remove()`.
+document.querySelector("#forever").remove();
 
 // ~ Create + Append a node
 // -> createElement(), append()
+// 1. Create an Element
+const pElement = document.createElement("p");
+// 2. Modify the new element.
+pElement.textContent = "On June 4, 2024";
+// 3. Append the new element to existing element.
+document.querySelector("#dates").append(pElement);
 
 // ~ Challenges
 // 1. Write a function named displayList that takes a name and an array,
@@ -41,6 +52,20 @@ Challenges
 //        Books
 //        • The Shining
 // 2. Replace the <strong> element with a newly created one.
+
+function displayList(name, books){
+const nameElement =  document.createElement("h2");
+nameElement.textContent = name
+const elementArray = document.createElement("ul");
+elementArray.textContent = books;
+document.querySelector("#dates").append(nameElement);
+document.querySelector("#nameElement").append(elementArray);
+}
+const name = "Books"
+const books = 
+
+displayList();
+
 
 
 
