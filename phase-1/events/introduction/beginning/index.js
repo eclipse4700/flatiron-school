01739a4d5ebc25f41a918253e910635b9fa115da
Isaiah <isaiahkantor23@gmail.com>
2024-06-06 11:43:22 -0400

    Updated Today

--- phase-1/events/introduction/beginning/index.js
+++ phase-1/events/introduction/beginning/index.js
@@ -17,11 +17,29 @@ Takeaways
 */
 
 // ~ the "click" event
+document.querySelector("#tomorrow").addEventListener("click",() => {
+    // Here, we decided what should happen when the "click" event fires on #tomorrow.
+    document.querySelector("#forever").textContent = "On June 5, 2024";
+});
+
+
 
 // ~ the "submit" event
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    // event === "submit"
+    // event.target === <form>
+    // event.target.date === <input name ="date">
+    // event.target.date.value === 2024-06-27
+    console.log(event.target.date.value);
+    document.querySelector("#sometime").textContent = event.target.date.value;
+});
 
 // ~ challenges
 // 1. [easy] Add another click event handler.
+
+
+
 // 2. [medium] Add some non-click, non-submit event listener to the page.
 // 3. [hard] Add an image, and a form with one text input to the page.
 //    On submit, have the image change to the URL specified in the text input.
